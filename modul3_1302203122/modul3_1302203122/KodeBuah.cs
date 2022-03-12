14746790f230ecc857df1da29a62a0bc27ffeed6
Rizky Ramdhan Nugraha <rizkyramdhan.117h@gmail.com>
2022-03-12 10:01:11 +0700

    menambahkan class KodeBuah

---
+++ modul3_1302203122/modul3_1302203122/KodeBuah.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul3_1302203122
+{
+    internal class KodeBuah
+    {
+        public string namaBuah;
+        public string kodeBuah;
+
+        public void getKodeBuah(string namaBuah, string kodeBuah)
+        {
+            this.namaBuah = namaBuah;
+            this.kodeBuah = kodeBuah;
+        }
+
+        
+    }
+}
--- modul3_1302203122/modul3_1302203122/Program.cs
+++ modul3_1302203122/modul3_1302203122/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul3_1302203122;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KodeBuah BUAH1 = new KodeBuah() { namaBuah = "Apel", kodeBuah = "A00" };
+            KodeBuah BUAH2 = new KodeBuah() { namaBuah = "Aprikot", kodeBuah = "B00" };
+            KodeBuah BUAH3 = new KodeBuah() { namaBuah = "Alpukat", kodeBuah = "C00" };
+            KodeBuah BUAH4 = new KodeBuah() { namaBuah = "Pisang", kodeBuah = "D00" };
+            KodeBuah BUAH5 = new KodeBuah() { namaBuah = "Paprika", kodeBuah = "E00" };
+            KodeBuah BUAH6 = new KodeBuah() { namaBuah = "Blackberry", kodeBuah = "F00" };
+            KodeBuah BUAH7 = new KodeBuah() { namaBuah = "Ceri", kodeBuah = "H00" };
+            KodeBuah BUAH8 = new KodeBuah() { namaBuah = "Kelapa", kodeBuah = "I00" };
+            KodeBuah BUAH9 = new KodeBuah() { namaBuah = "Jagung", kodeBuah = "J00" };
+            KodeBuah BUAH10 = new KodeBuah() { namaBuah = "Kurma", kodeBuah = "K00" };
+            KodeBuah BUAH11 = new KodeBuah() { namaBuah = "Durian", kodeBuah = "L00" };
+            KodeBuah BUAH12 = new KodeBuah() { namaBuah = "Anggur", kodeBuah = "M00" };
+            KodeBuah BUAH13 = new KodeBuah() { namaBuah = "Melon", kodeBuah = "N00" };
+            KodeBuah BUAH14 = new KodeBuah() { namaBuah = "Semangka", kodeBuah = "O00" };
+
+
+
+        }
+    }
+}
